2a81abdc2c6a04c2a299ed855ff603940ec0b94a
= <petitalx@gmail.com>
2018-08-20 18:25:09 +0200

    prototyping all in canvas

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -1,8 +1,5 @@
 <template>
   <div id="wrapper">
-    <video id="video" loop> <!-- controls="controls"-->
-      <source src="/static/mg.mp4"/>
-    </video>
     <div id="overlay-wrapper">
       <canvas ref="overlay"></canvas>
     </div>
@@ -18,7 +15,7 @@
   const BRUSH_SCALE_MAX = 2.0
   const BRUSH_SCALE_INC = 0.1
 
-  const MASK_ALPHA = 0.5
+  // const MASK_ALPHA = 0.5
 
   export default {
     name: 'landing-page',
@@ -32,8 +29,8 @@
         resources: null,
         overlay: null,
         dragging: false,
+        lastTime: -1,
         scale: 1.0,
-        blurFilter: null,
         state: {
           erasing: false
         }
@@ -56,34 +53,31 @@
       preload () {
         console.log('@preload')
         let datas = [
-          {name: 'brush', url: '/static/brush_b.png'}
+          {name: 'brush', url: '/static/brush_b.png'},
+          {name: 'test-movie', url: '/static/mg.mp4'}
         ]
         PIXI.loader.add(datas)
         PIXI.loader.load(this.initialize)
       },
       initialize (loader, resources) {
-        console.log('@initialize')
+        console.log('@initialize', loader, resources)
+        window.resources = resources
+        window.loader = loader
+
         this.resources = resources
         this.helperCanvas = document.createElement('canvas')
-        this.video = document.getElementById('video')
-        this.loadVideo('/static/mg.mp4')
-      },
-      loadVideo (src) {
-        this.video.oncanplay = this.initializePIXI
-        this.video.childNodes[0].src = src // + new Date().getTime()
+        this.initializePIXI()
       },
       //
       // - PIXI
       //
       initializePIXI () {
         console.log('@initializePIXI')
-        this.video.oncanplay = null
-
-        let w = this.video.videoWidth
-        let h = this.video.videoHeight
+        let w = 1280 // this.video.videoWidth
+        let h = 720 // this.video.videoHeight
         const c = this.$refs.overlay
-        c.width = w
-        c.height = h
+        // c.width = w
+        // c.height = h
 
         this.PIXIApp = new PIXI.Application(w, h, {
           view: c,
@@ -92,21 +86,68 @@
 
         let stage = this.PIXIApp.stage
 
-        let brush = new PIXI.Container()
-        let brushAsset = new PIXI.Sprite(this.resources['brush'].texture)
-        brush.addChild(brushAsset)
-        brushAsset.anchor.x = 0.5
-        brushAsset.anchor.y = 0.5
-        this.brush = brush
-        this.brushAsset = brushAsset
-        this.brushSetErase(true)
+        // Create the brush
 
+        // let brush = new PIXI.Container()
+        // let brushAsset = new PIXI.Sprite(this.resources['brush'].texture)
+        // brush.addChild(brushAsset)
+        // brushAsset.anchor.x = 0.5
+        // brushAsset.anchor.y = 0.5
+        // this.brush = brush
+        // this.brushAsset = brushAsset
+        // this.brushSetErase(true)
+
+        // Create video playback layer
+
+        /*
+        // create a video texture from a path
+        var texture = PIXI.Texture.fromVideo('required/assets/testVideo.mp4')
+        // create a new Sprite using the video texture (yes it's that easy)
+        var videoSprite = new PIXI.Sprite(texture)
+        // Stetch the fullscreen
+        videoSprite.width = w
+        videoSprite.height = h
+        stage.addChild(videoSprite)
+        */
+
+        /*
+        // eslint-disable-next-line
+        let videoTexture = new PIXI.VideoBaseTexture.fromUrl('test-movie')
+        // eslint-disable-next-line
+        let video = new PIXI.Sprite.from(videoTexture)
+        video.width = w
+        video.height = h
+        stage.addChild(video)
+        videoTexture.source.play()
+        */
+        let videoTexture = PIXI.Texture.fromVideo('test-movie')
+        // eslint-disable-next-line
+        let video = new PIXI.Sprite(videoTexture)
+        video.width = w
+        video.height = h
+        stage.addChild(video)
+        videoTexture.baseTexture.source.play()
+
+        console.log(videoTexture.hasLoaded)
+        window.videoTexture = videoTexture
+
+        // Create (w x h) black and white texture.
+        // Initially, the masked area is totally blank
+        /*
         this.maskTexture = PIXI.RenderTexture.create(w, h)
         let t = new PIXI.Graphics()
         t.beginFill(0xFFFFFF)
         t.drawRect(0, 0, w, h)
         t.endFill()
         this.PIXIApp.renderer.render(t, this.maskTexture)
+
+        // Create a sprites from the maskTexture
+        // - maskTextureSprite will be applied as effective mask.
+        //   It has a plain opacity and presents maskTexture as it is.
+        // - maskTextureVisual will be displayed.
+        //   It has a reduced opacity and will provide a feedback to the user,
+        //   helping him to draw the shape of the mask.
+
         this.maskTextureSprite = new PIXI.Sprite(this.maskTexture)
         this.maskTextureVisual = new PIXI.Sprite(this.maskTexture)
         this.maskTextureVisual.alpha = MASK_ALPHA
@@ -116,12 +157,19 @@
         stage.on('pointerdown', this.pointerDown)
         stage.on('pointerup', this.pointerUp)
         stage.on('pointermove', this.pointerMove)
+        */
       },
+      animate () {
+        // render the stage
+        this.PIXIApp.renderer.render(this.PIXIApp.stage)
+        requestAnimationFrame(this.animate)
+      },
+      // - Handle brush events
       pointerMove (event) {
         if (this.dragging) {
           console.log('pointerMove')
           this.brush.position.copy(event.data.global)
-          this.PIXIApp.renderer.render(this.brush, this.maskTexture, false, null, false)
+          // this.PIXIApp.renderer.render(this.brush, this.maskTexture, false, null, false)
         }
       },
       pointerDown (event) {
@@ -171,7 +219,47 @@
           console.warn('videoNextFrame: can\'t jump next frame while playing')
         }
       },
+      preview () {
+        console.log('@preview')
+        this.video.currentTime = 0
+        this.video.play()
+        // this.draw()
+      },
+      /*
+      draw () {
+        let time = this.video.currentTime
+        if (time !== this.lastTime) {
+          console.log('time: ' + time)
+          // todo: do your rendering here
+
+          // // -----------------------------------------------------------
+          // // -----------------------------------------------------------
+          // this.video.pause()
+          // console.log('@capture')
+          // let c = this.helperCanvas
+          // c.width = this.video.videoWidth
+          // c.height = this.video.videoHeight
+          // c.getContext('2d').drawImage(this.video, 0, 0, c.width, c.height)
+          // if (this.playback) {
+          //   this.playback.destroy()
+          // }
+          // this.playback = PIXI.Sprite.fromImage(c.toDataURL())
+          // // this.playback.mask = this.maskTextureSprite
+          // this.PIXIApp.stage.addChildAt(this.playback, 0)
+          // this.video.play()
+          // // -----------------------------------------------------------
+          // // -----------------------------------------------------------
+
+          this.lastTime = time
+        }
+        // wait approximately 16ms and run again
+        requestAnimationFrame(this.draw)
+      },
+      */
       capture () {
+        // capture : set the current video frame as the new still frame picture.
+        // If a still frame is yet setted, destroy it's sprite to create a new one
+        // Then add it on top of all elements
         console.log('@capture')
         let c = this.helperCanvas
         c.width = this.video.videoWidth
@@ -194,6 +282,7 @@
         this.maskTextureVisual.visible = !this.maskTextureVisual.visible
         console.log('toggleMask:', this.maskTextureVisual.visible)
       },
+
       brushScale (scale) {
         console.log('brushScale:', scale)
         this.brushAsset.setTransform(0, 0, scale, scale, 0, 0, 0, 0, 0)
@@ -253,6 +342,7 @@
     },
     mounted () {
       this.commands = {
+        'p': this.preview,
         'a': this.videoTogglePlay,
         'z': this.videoPrevFrame,
         'e': this.videoNextFrame,
@@ -282,7 +372,10 @@
     padding: 0;
   }
 
-  body { font-family: 'Source Sans Pro', sans-serif; }
+  body { 
+    font-family: 'Source Sans Pro', sans-serif; 
+    background-color: 'rebeccapurple';
+  }
 
   #wrapper {
     height: 100vh;
